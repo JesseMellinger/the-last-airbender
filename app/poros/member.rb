4cf835db4f03225f8fa7002fda11a4fef8298031
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-11 10:31:29 -0700

    Create additional attribute for image url of member

--- app/poros/member.rb
+++ app/poros/member.rb
@@ -1,19 +1,21 @@
 class Member
   attr_reader :name,
-              :affiliation
+              :affiliation,
+              :photo
 
   def initialize(member_data)
     @allies = member_data[:allies]
     @enemies = member_data[:enemies]
     @name = member_data[:name]
     @affiliation = member_data[:affiliation]
+    @photo = member_data[:photoUrl]
   end
 
   def allies
-    @allies.join('&')
+    @allies.length > 1 ? @allies.join(' & ') : @allies.join
   end
 
   def enemies
-    @enemies.join('&')
+    @enemies.length > 1 ? @enemies.join(' & ') : @enemies.join
   end
 end
