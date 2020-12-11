5cd4fff04c58f3d365028e1cf2ba00e31cde7b3f
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-11 09:58:59 -0700

    Update to test for attributes rendered correctly

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -16,7 +16,7 @@ require 'rails_helper'
 
 describe 'as a user' do
   describe 'when I visit / and I select "Fire Nation" from the select field and click "Search for Members"' do
-    it 'then I should be on the page "/search", I should see the total number of people who live in the Fire Nation, and I should see a list with the detailed information for all 97 members of the Fire Nation' do
+    it 'then I should be on the page "/search", I should see the total number of people who live in the Fire Nation, and I should see a list with the detailed information for all 20 members of the Fire Nation' do
       visit root_path
 
       select 'Fire Nation', :from => :nation
@@ -25,16 +25,15 @@ describe 'as a user' do
 
       expect(current_path).to eq(search_path)
 
-      expect(page).to have_content("Total Members: 97")
+      expect(page).to have_content("Total Members: 20")
 
-      expect(page).to have_css('.members', count: 97)
+      expect(page).to have_css('.members', count: 20)
 
       within(first('.members')) do
-        # expect(page).to have_content("Name of park: Andrew Johnson National Historic Site")
-        # expect(page).to have_content("Description: Andrew Johnson's complex presidency (1865-69) illustrates the Constitution at work following the Civil War. As the President and Congress disagreed on Reconstruction methods, the Constitution served as their guide on balance of powers, vetoes, and impeachment. In the end, it evolved as a living document with pivotal amendments on freedom, citizenship, and voting rights - topics still vital today.")
-        # expect(page).to have_content("Directions: GPS The GPS setting for Andrew Johnson NHS may be listed as 121 Monument Ave, which is the park HQ in the National Cemetery. To arrive at the Visitor Center, use 101 North College Street, Greeneville, TN. Plane The closest airport is the Tri-Cities Regional Airport, 43 miles NE of Greeneville. From the airport, take I-81 South to exit 36 and follow the signs to Greeneville. Car From I-81S take exit 36 to Rt. 172 south to Greeneville. From I-81N take exit 23 to Rt. 11E north to Greeneville.")
-        # expect(page).to have_content("Wednesday: 9:00AM - 4:00PM")
-        # expect(page).to have_content("Monday: Closed")
+        expect(page).to have_content("Name: Chan (Fire Nation admiral)")
+        expect(page).to have_content("Affiliation: Fire Nation Navy")
+        expect(page).to have_content("Allies: Ozai")
+        expect(page).to have_content("Enemies: Earth Kingdom")
       end
     end
   end
