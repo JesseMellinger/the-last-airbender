1a6c5301bc5f19a669d0ebf6ed4a832ab5e34d96
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-11 09:57:54 -0700

    Update param format before sending to service

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,5 +1,6 @@
 class SearchFacade
   def self.get_members(nation)
+    nation = nation.split('_').join('+')
     json = AirbenderDataService.members_of_nation(nation)
     json.map do |member_data|
       Member.new(member_data)
