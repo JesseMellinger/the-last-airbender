fcbc84eb8fcabdb9effbb781f2ccb527e2b3b6fd
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-12-11 10:32:10 -0700

    Update to test for presence of image

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
-# As a user,
-# When I visit "/"
-# And I Select "Fire Nation" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members“
-# Then I should be on page “/search”
-# Then I should see the total number of people who live in the Fire Nation. (i.e. there are 97 total)
-# And I should see a list with the detailed information for all 97 members of the Fire Nation.
-#
-# And for each of the members I should see:
-# - The name of the member (and their photo, if they have one)
-# - The list of allies and a list of enemies (if they have any)
-# - Any affiliation that the member has (if they have any)
-
 describe 'as a user' do
   describe 'when I visit / and I select "Fire Nation" from the select field and click "Search for Members"' do
     it 'then I should be on the page "/search", I should see the total number of people who live in the Fire Nation, and I should see a list with the detailed information for all 20 members of the Fire Nation' do
@@ -34,6 +20,14 @@ describe 'as a user' do
         expect(page).to have_content("Affiliation: Fire Nation Navy")
         expect(page).to have_content("Allies: Ozai")
         expect(page).to have_content("Enemies: Earth Kingdom")
+      end
+
+      within all('.members')[1] do
+        expect(page).to have_content("Name: Circus master")
+        expect(page).to have_content("Affiliation: Fire Nation circus")
+        expect(page).to have_content("Allies: Ty Lee")
+        expect(page).to have_content("Enemies: Appa")
+        expect(page).to have_xpath("//img[contains(@src,'https://vignette.wikia.nocookie.net/avatar/images/a/a5/Circus_master.png/revision/latest?cb=20130706153819')]")
       end
     end
   end
